7bf37079164445680e6df965bd3047b33ad10c50
Sage Raflik <neonfuz@gmail.com>
2021-05-21 14:19:25 -0500

    Changes

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,22 @@
 <script>
-	import Board from './Board.svelte';
+ import Board from './Board.svelte';
 </script>
 
 <div class="container">
-	<Board n={9} />
+	<Board n={9} width="100%" height="100%" />
 	<Board n={13} />
 	<Board />
 </div>
 
 <style>
-	.container {
-		display: flex;
-		flex-basis: 50%;
-		flex-wrap: wrap;
-	}
+ :global(body) {
+	 overflow: hidden;
+ }
+ .container {
+	 display: flex;
+	 flex-basis: 50%;
+	 flex-wrap: wrap;
+	 overflow: hidden;
+	 max-height: 80vh;
+ }
 </style>
