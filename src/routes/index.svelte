b68a8c090da121652be525b6a6f4279661f3b2eb
Sage Raflik <neonfuz@gmail.com>
2021-05-23 11:31:03 -0500

    Moved game to /play/ subroute, working on mobile touch

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,3 @@
-<div class="container">
-    <Game />
-</div>
-
-<style>
- .container {
-     max-width: calc(100vmin - 20px);
- }
-</style>
-
-<script>
- import Game from '$lib/Game.svelte';
-</script>
+<a href="/play/">
+    Play
+</a>
---
+++ src/routes/play.svelte
@@ -0,0 +1,15 @@
+<div class="container">
+    <Game />
+</div>
+
+<style>
+ .container {
+     max-width: calc(100vmin - 20px);
+     margin: 10px auto;
+     height: 100vh;
+ }
+</style>
+
+<script>
+ import Game from '$lib/Game.svelte';
+</script>
