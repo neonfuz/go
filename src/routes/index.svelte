4ff76e62e8b8067766b067f40d363627d73fb79a
Sage <neonfuz@gmail.com>
2022-07-28 15:10:11 -0500

    Moved game into index route

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,15 @@
-<a href="/play/">
-    Play
-</a>
+<script>
+ import Game from '$lib/Game.svelte';
+</script>
+
+<div class="container">
+    <Game />
+</div>
+
+<style>
+ .container {
+     max-width: calc(100vmin - 20px);
+     margin: 10px auto;
+     height: 100vh;
+ }
+</style>
